61395bda402f79b954d45ad4556bf24861ac333a
Pranali Prakash Pansare <89742255+pranalipansare99@users.noreply.github.com>
2022-08-29 17:23:59 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
+import '../dist/index.html';
 
 const App = () => {
   return (
     <div id="main"></div>
+    <h2></h2>
+    <p></p>
   )
 }
 
