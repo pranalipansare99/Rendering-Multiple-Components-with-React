21cf7f11cb560c61f6ba44a0c23e40ec1b431fe3
Pranali Prakash Pansare <89742255+pranalipansare99@users.noreply.github.com>
2022-08-29 17:35:09 +0530

    Update parser.js

--- parser.js
+++ parser.js
@@ -7,3 +7,10 @@ const options = {compact: true, ignoreComment: true, spaces: 4};
 const result = parser.xml2json(xml, options)
 
 console.log(result);
+
+export default function Header(){
+  return (
+    <h2 data-ns-test="project-name">Calculator</h2>
+    <p  data-ns-test="project-description"">This is my First app</p>
+    )
+}
