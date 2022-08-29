e752058c8b616b0cb0aa243a8bd9d23e4706ca63
Pranali Prakash Pansare <89742255+pranalipansare99@users.noreply.github.com>
2022-08-30 00:55:20 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
-import Header from'./SectionComp';
-import Section1 from'./HeaderComp';
+import Header from'../../SectionComp';
+import Section1 from'../../HeaderComp';
 
 const App = () => {
   return (
